580398ea16f3388eb24b0c4c94333c81bb31cc64
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2019-10-31 11:06:51 +0500

    Add model JobPlace

--- web/src/register/models.py
+++ web/src/register/models.py
@@ -8,6 +8,16 @@ class User(AbstractUser):
     patronymic = models.CharField(max_length=30, blank=False)
     email = models.EmailField(max_length=70, blank=False)
     experience = models.PositiveSmallIntegerField(default=0)
+    job_place = models.ForeignKey('JobPlace', on_delete=models.DO_NOTHING,
+                                  related_name='users',
+                                  null=True)
 
     def __str__(self):
         return f'Username: {self.username}, email: {self.email}'
+
+
+class JobPlace(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'Наименование: {self.name}'
