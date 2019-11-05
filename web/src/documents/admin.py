55d5ccdefabaabc91f69505acb5648c283df5bc6
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2019-11-05 11:29:24 +0500

    Add documents models to admin

--- web/src/documents/admin.py
+++ web/src/documents/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, DocumentType, DocumentRegulation, Contract
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('number', 'file', 'user', 'document_type')
+    list_display_links = ('number',)
+    search_fields = ('number',)
+
+    class Meta:
+        model = Document
+
+
+@admin.register(DocumentType)
+class DocumentTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event', 'get_documents')
+    list_display_links = ('name',)
+    list_filter = ('event',)
+    search_fields = ('name',)
+
+    def get_documents(self, obj):
+        documents = Document.objects.filter(document_type=obj)
+        return ' | '.join(str(document) for document in documents)
+
+    get_documents.short_description = 'Документы'
+
+    class Meta:
+        model = DocumentType
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('number', 'date', 'status', 'document_regulation', 'user')
+    list_display_links = ('number',)
+    list_filter = ('date', 'status')
+    search_fields = ('number',)
+
+    class Meta:
+        model = Contract
+
+
+@admin.register(DocumentRegulation)
+class DocumentRegulationAdmin(admin.ModelAdmin):
+    list_display = ('document_type', 'get_contracts')
+    list_display_links = ('document_type',)
+    search_fields = ('document_type',)
+
+    def get_contracts(self, obj):
+        contracts = Contract.objects.filter(document_regulation=obj)
+        return ' | '.join(str(contract) for contract in contracts)
+
+    get_contracts.short_description = 'Договоры'
+
+    class Meta:
+        model = DocumentRegulation
