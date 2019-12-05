c36824c2318d6053e33d3ca2d45669b02cbb98ff
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2019-12-06 00:46:06 +0500

    Update tests for model User

--- web/src/register/tests/test_models.py
+++ web/src/register/tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.models import Group
 
 from ..models import User, AdditionalUserInfo, JobPosition, JobPlace
 
@@ -7,7 +8,8 @@ from ..models import User, AdditionalUserInfo, JobPosition, JobPlace
 class TestUserModel:
     @pytest.fixture(autouse=True)
     def setup(self):
-        User.objects.create(username='oleg',
+        User.objects.create(username='oleg', first_name='Олег',
+                            last_name='Иванов', patronymic='Степанович',
                             password='123456789qwe$')
 
     def test_fields_max_length(self):
@@ -21,6 +23,34 @@ class TestUserModel:
         user = User.objects.first()
         assert user._meta.get_field('email_confirmed').default is False
         assert user._meta.get_field('experience').default == 0
+
+    def test_get_full_name_returns_valid_value(self):
+        user = User.objects.first()
+        expected = 'Иванов Олег Степанович'
+        actual = user.get_full_name()
+        assert expected == actual, \
+            f'Expected({expected}) is not equal to actual({actual})'
+
+    def test_activate_user_accout_after_email_confirm(self):
+        user = User.objects.first()
+        user.activate_user_accout_after_email_confirm()
+        assert user.is_active is True
+        assert user.email_confirmed is True
+        assert user.has_usable_password()
+
+    def test_add_user_to_group_listeners(self):
+        user = User.objects.first()
+        user.add_user_to_group_listeners()
+        group = Group.objects.get(name='Слушатели')
+        assert user in group.user_set.all(), \
+            f'User is not in group {group.name}'
+
+    def test_add_user_to_group_teachers(self):
+        user = User.objects.first()
+        user.add_user_to_group_teachers()
+        group = Group.objects.get(name='Преподаватели')
+        assert user in group.user_set.all(), \
+            f'User is not in group {group.name}'
 
 
 @pytest.mark.django_db
