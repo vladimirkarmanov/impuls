38d9d3d4762ed81d68fc89723294bb3232480944
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2020-01-16 00:41:04 +0500

    Update production settings

--- web/src/impuls/settings/production.py
+++ web/src/impuls/settings/production.py
@@ -19,18 +19,19 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'connection.cnf'),
-        }
+        'NAME': 'impuls_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': 3306,
+        'CHARSET': 'utf8',
+        'COLLATION': 'utf8_bin',
     }
 }
 
-# Media and statifiles
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.mailgun.org'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env.str('EMAIL_HOST')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
