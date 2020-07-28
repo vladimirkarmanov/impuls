80908d33ef3c3e210ef7c62966e6e6d775038620
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2020-07-28 12:02:36 +0500

    Update UserFactory

--- backend/register/tests/factories/user.py
+++ backend/register/tests/factories/user.py
@@ -1,4 +1,5 @@
 import factory
+from django.contrib.auth.hashers import make_password
 from factory import fuzzy
 from factory.django import DjangoModelFactory
 
@@ -6,12 +7,15 @@ from register.models import User
 from register.tests.factories.job_place import JobPlaceFactory
 from register.tests.factories.job_position import JobPositionFactory
 
+USER_PASSWORD = "ipS#@&3o23"
+
 
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
 
     username = factory.Faker('user_name')
+    password = make_password(USER_PASSWORD)
     first_name = factory.Faker('first_name', locale='ru_RU')
     last_name = factory.Faker('last_name', locale='ru_RU')
     patronymic = factory.Faker('middle_name', locale='ru_RU')
