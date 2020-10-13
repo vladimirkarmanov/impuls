594bd9e2796be551d4fc2a543d150e78a8a16274
vladimirkarmanov <vladimir1998karmanov@gmail.com>
2020-10-13 12:44:38 +0500

    Add index page

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from config.views import index
+
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('register/', include(('register.urls', 'register'), namespace='register')),
     path('events/', include(('events.urls', 'events'), namespace='events'))
