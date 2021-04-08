b96e15ccc0a52a2efdb84ddd68b8a882973090d7
Parfenova, Vera <vera.parfenova@clarivate.com>
2021-04-08 10:53:53 +0500

    add model course

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Course
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'short_name')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+
+    class Meta:
+        model = Course
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+
+
+class Course(models.Model):
+    number = models.IntegerField(verbose_name='Номер')
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    short_name = models.CharField(max_length=50, verbose_name='Краткое название')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
