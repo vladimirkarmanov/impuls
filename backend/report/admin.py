e6343cb2296f45724c2f0851bf11066a854b2895
Parfenova, Vera <vera.parfenova@clarivate.com>
2021-04-15 12:17:16 +0500

    add TrainingDocument model

--- backend/report/admin.py
+++ backend/report/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import TrainingDocument
+
+
+@admin.register(TrainingDocument)
+class TrainingDocumentAdmin(admin.ModelAdmin):
+    list_display = ('number',)
+    list_display_links = ('number',)
+    search_fields = ('number', 'date')
+
+    class Meta:
+        model = TrainingDocument
--- backend/report/models.py
+++ backend/report/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+
+
+class TrainingDocument(models.Model):
+    number = models.PositiveIntegerField(verbose_name='Номер')
+    date = models.DateField(verbose_name='Дата выдачи')
+    number_of_hours = models.DecimalField(max_digits=5, decimal_places=1, verbose_name='Количество часов')
+
+    class Meta:
+        verbose_name = 'Документ о повышении квалификации'
+        verbose_name_plural = 'Документы о повышении квалификации'
+        ordering = ['date']
+
+    def __str__(self):
+        return str(self.number)
