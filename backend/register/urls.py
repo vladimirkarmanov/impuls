d6b6160f0f64a90f931e9889bf55630325ff4700
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2021-05-23 23:37:16 +0500

    add students list

--- backend/register/urls.py
+++ backend/register/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserLoginView, UserLogoutView
+from .views import UserLoginView, UserLogoutView, StudentsList
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user_login'),
     path('logout/', UserLogoutView.as_view(), name='user_logout'),
+    path('students/', StudentsList.as_view(), name='students_list'),
 ]
--- backend/register/views.py
+++ backend/register/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.views import (LoginView,
-                                       LogoutView)
+from django.contrib.auth.views import LoginView, LogoutView
 from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
 from core.decorators import login_not_required
-from .forms import (UserLoginForm)
+from register.models.TeacherRole import TeacherRole
+from .forms import UserLoginForm
 from .mixins import UserAlreadyAuthenticatedMixin
 
 
@@ -16,3 +17,13 @@ class UserLoginView(UserAlreadyAuthenticatedMixin, LoginView):
 @method_decorator(login_not_required, name='dispatch')
 class UserLogoutView(LogoutView):
     pass
+
+
+class StudentsList(ListView):
+    model = TeacherRole
+    template_name = 'register/students_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['students'] = TeacherRole.objects.all()
+        return context
